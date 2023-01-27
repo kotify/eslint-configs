02a5af088f8400a357e791944a08a6967d6cf8d1
Konstantin Alekseev <mail@kalekseev.com>
2023-01-27 18:20:54 +0200

    Bump deps, stricter rules

--- packages/eslint-config-typescript-react/index.js
+++ packages/eslint-config-typescript-react/index.js
@@ -13,11 +13,14 @@ module.exports = {
     "plugin:react-hooks/recommended",
     "prettier",
   ],
-  settings: { react: { version: "17.0" } },
   rules: {
     "react/jsx-uses-react": 0,
     "react/react-in-jsx-scope": 0,
     "react/prop-types": 0,
+    "react/no-unknown-property": ["error", { ignore: ["css"] }],
+    "@typescript-eslint/no-explicit-any": ["error", { ignoreRestArgs: true }],
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/consistent-type-imports": "error",
     "@typescript-eslint/no-useless-constructor": prodErrorDevWarning,
     "@typescript-eslint/no-unused-vars": [
       prodErrorDevWarning,
